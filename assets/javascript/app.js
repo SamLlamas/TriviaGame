c445c492969641307e46f5d657042427c92c575c
Sam Llamas <sam.llamas@utexas.edu>
2018-04-26 19:10:05 -0500

    working

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+$( document ).ready(function() {
+    var currentQuestion = 0;
+    var correct  = 0;
+    var wrong = 0;
+
+    var questions = ["Do you have the time?", "to listen to me whine", "about everything and nothing at all"]
+
+    var answers = [ "no", "nope", "stop"]
+    var fakes = [["yes", "yea sure", "so down"], ["I guess, doesn't sound bad", "isn't this a song"], ["you know what?...", "yea I'm out", "this is definitely a song..."]]
+
+    for (i = 0; i<questions.length; i++){
+        
+    }
+
+
+    
+
+    
+});
